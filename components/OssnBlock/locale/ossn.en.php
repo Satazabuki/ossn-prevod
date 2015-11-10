bb2289259b57b6a33b67984c771e00822536728d
Satazabuki <budjicaaa@gmail.com>
2015-11-11 00:53:33 +0100

    Create ossn.en.php

---
+++ components/OssnBlock/locale/ossn.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+    'user:blocked' => 'Korisnike je blokira!',
+    'user:block:error' => 'Kosinik ne može biti blokiran! Molimo pokušajte kasnije.',
+    'user:block' => 'Blokira',
+    'user:unblock' => 'Odblokiraj',
+);
+ossn_register_languages('en', $en); 
