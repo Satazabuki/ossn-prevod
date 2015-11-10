7f83c2f5f9c45fa905f1e602d6336249d9be80d7
Satazabuki <budjicaaa@gmail.com>
2015-11-10 22:50:04 +0100

    Create ossn.en.php

---
+++ components/OssnInvite/locale/ossn.en.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+	'com:ossn:invite' => 'Pozovi',			
+	'com:ossn:invite:friends' => 'Pozovite prijatelje',
+	'com:ossn:invite:friends:note' => 'Da pozovete prijatelja potrebno je uneti njihovu mail adresu. Nakon toga oni će dobiti dobiti mail sa pozivnicom.',
+	'com:ossn:invite:emails:note' => 'Email adrese (odvojene yareyom)',
+	'com:ossn:invite:emails:placeholder' => 'primer1@mail.com, primer2@mail.com',
+	'com:ossn:invite:message' => 'Poruka',
+		
+    	'com:ossn:invite:mail:subject' => 'Pozivnica da se pridružite %s',	
+    	'com:ossn:invite:mail:message' => 'Pozvani ste da se pridružite na %s od %s. Uz to imate i poruku:
+
+%s
+
+Da se pridružite kliknite na:
+
+%s
+
+Profil link: %s
+',	
+	'com:ossn:invite:mail:message:default' => 'Hi,
+
+Želim da mi se pridruyis na mreži %s.
+
+Profil link : %s
+
+Srdačan pozdrav.
+%s',
+	'com:ossn:invite:sent' => 'Vaši prijatelji su pozvani. Pozivnica poslata: %s.',
+	'com:ossn:invite:wrong:emails' => 'Ove mail adrese nisu validne: %s.',
+	'com:ossn:invite:sent:failed' => 'Ne mogu pozvati korisnika sa email adresom: %s.',
+	'com:ossn:invite:already:members' => 'Ova adresa se je već u upotrebi: %s',
+	'com:ossn:invite:empty:emails' => 'Molimo vas dodajte barem jednu email adresu',
+);
+ossn_register_languages('en', $en); 
