80f36506874fd39ace6cbf82541a4c19c4096e31
Satazabuki <budjicaaa@gmail.com>
2015-11-10 23:02:49 +0100

    Create ossn.en.php

---
+++ components/OssnMessages/locale/ossn.en.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+    'user:messages' => 'Poruke',
+    'inbox' => 'Sanduče',
+    'send' => 'Pošalji',
+    'ossn:message:between' => 'Poruke %s',
+    'messages' => 'Poruke',
+    'message:placeholder' => 'Unesi poruku',
+    'no:messages' => 'Nema novih poruka.'
+);
+ossn_register_languages('en', $en); 
